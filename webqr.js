265fb88e2872ac22912b7c3a07adc1b90679e845
Rounak1105 <64644449+Rounak1105@users.noreply.github.com>
2020-12-31 10:57:25 +0530

    Update webqr.js

--- webqr.js
+++ webqr.js
@@ -231,3 +231,26 @@ function setimg() {
     qrfile.addEventListener("drop", drop, false);
     stype = 2;
 }
+
+cameraOptions.onchange = () => {
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+  
+    startStream(updatedConstraints);
+  };
+
+const getCameraSelection = async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    const options = videoDevices.map(videoDevice => {
+      return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+    });
+    cameraOptions.innerHTML = options.join('');
+  };
+  
+  getCameraSelection();
+  
